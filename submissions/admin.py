8b242fd1333ef50e674858e668fa092a82320768
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2025-01-14 17:24:39 +0200

    Register all models to admin site

--- submissions/admin.py
+++ submissions/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Course, User
+from .models import Course, User, BaseCourse, Exercise, Student, Feedback
 
 admin.site.register(Course)
 admin.site.register(User, UserAdmin)
+admin.site.register(BaseCourse)
+admin.site.register(Exercise)
+admin.site.register(Student)
+admin.site.register(Feedback)
